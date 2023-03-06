c565fba4888b0094f3200d8aa11dd028ed2c4f15
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-03-06 16:18:25 -0300

    FEAT: REQ 7 DONE

---
+++ src/models/Category.js
@@ -0,0 +1,28 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+module.exports = (sequelize, DataTypes) => {
+
+    const Category = sequelize.define('Category',
+    {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: DataTypes.INTEGER,
+          },
+          name: {
+            allowNull: false,
+            type: DataTypes.STRING,
+          },
+    },
+    {timestamps: false,
+      underscored: true,
+     tableName: 'categories'    
+    }
+    );
+
+    return Category
+}
