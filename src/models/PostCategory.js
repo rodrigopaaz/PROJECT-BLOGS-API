768c475a8731ad2ee1a7694b288c290ee0752738
rodrigo <rodrigo_pazlimas@hotmail.com>
2023-03-06 23:43:00 -0300

    FEAT REQ 11 DONE

---
+++ src/models/PostCategory.js
@@ -0,0 +1,43 @@
+/**
+ * @param {import('sequelize').Sequelize} sequelize
+ * @param {import('sequelize').DataTypes} DataTypes
+ */
+
+module.exports = (sequelize, DataTypes) => {
+
+    const PostCategory = sequelize.define('PostCategory',
+    {
+        postId: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'BlogPost',
+                key: 'id'
+            }
+          },
+          categoryId: {
+            allowNull: false,
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Category',
+                key: 'id'
+            }
+          },
+    },
+    {timestamps: false,
+      underscored: true,
+     tableName: 'post_categories'    
+    }
+    );
+    PostCategory.associate = (models) => {
+        models.Category.belongsToMany(models.BlogPost, {
+            as: 'blog_post', through: PostCategory,
+            foreignKey: 'categoryId', otherKey: 'postId',
+        });
+        models.BlogPost.belongsToMany(models.Category, {
+            as: 'categories', through: PostCategory,
+            foreignKey: 'postId', otherKey: 'categoryId',
+        });
+    }
+    return PostCategory
+}
